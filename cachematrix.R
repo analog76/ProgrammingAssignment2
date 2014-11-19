dd641501603167f5fde5a75e853d02e281469868
Anandha Rangantahan <analog76@Anandhas-MacBook-Pro.local>
2014-11-18 21:28:51 -0800

    Programming assignment2'

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to calculate the inverse of matrix and stores its cache value.
 
-## Write a short comment describing this function
-
+ 
+## A special matrix object has been created to  cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
 
+		inverseMatrix=NULL
+		
+		## method to set the matrix and re-initialize the inverseMatrix to NULL
+ 		set<-function(z){
+ 			m<<-z
+ 			inverseMatrix= NULL
+ 		}
+ 		
+ 		## method to get the matrix 
+ 		get<-function(){
+ 			m
+ 		}
+ 		
+ 		## it stores the value in the cache.
+ 		setInverseMatrix <- function(im){
+ 			inverseMatrix <<- im	
+ 		}
+ 		
+ 		## method to get the inverse matrix.
+ 		getInverseMatrix <- function(){
+ 			inverseMatrix
+ 		}
+ 		
+ 		## returns list of methods.
+ 		list(set=set,
+ 			 get=get,
+ 			 setInverseMatrix=setInverseMatrix,
+ 			 getInverseMatrix=getInverseMatrix)
+ 
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    ## Retrieves inverse of matrix from makeCacheMatrix object.
+    inverseMatrix <- x$getInverseMatrix()
+    
+    
+    ## checks if the inverse of matrix is NULL or not.
+    ## if it is NOT null returns that value.
+	if(!is.null(inverseMatrix)){
+		message(" getting cached inverse matrix ")
+		return(inverseMatrix)
+	}
+	
+	## if the inverse of matrix is NULL then if block will not get executed.
+	## it calculates the inverse of matrix here. 
+	data <- x$get()
+	inverseMatrix <- solve(data,...)
+
+	## caches the value in the makeCacheMatrix object.
+	x$setInverseMatrix(inverseMatrix)
+	
+	## returns the value .
+	inverseMatrix
 }
